eb47ccb08057299b4ad8bf3ddbcc877bb18ee56a
ash_tree4314 <ash.tree4314@gmail.com>
2024-12-03 10:15:22 +0300

    add: CustomCleanClient

---
+++ AddressStandartization.Api/Controllers/Models/AddressRequestModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AddressStandartization.Api.Controllers.Models;
+
+public class AddressRequestModel
+{
+    public string Address { get; set; } = string.Empty;
+}
---
+++ AddressStandartization.Services/Interfaces/ICustomCleanClient.cs
@@ -0,0 +1,8 @@
+﻿using AddressStandartization.Services.Services.Models;
+
+namespace AddressStandartization.Services.Services.Interfaces;
+
+public interface ICustomCleanClient
+{
+    Task<string> CleanAddressAsync(AddressModel address);
+}
